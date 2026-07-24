b2fa7100745d0cc71e7e20ea9ac3d79452bbdb7f
fadhilarrayyan <arrayyanfadhilm@gmail.com>
2026-07-24 10:30:32 +0700

    Add files via upload

---
+++ royan.js
@@ -0,0 +1,6 @@
+const nama = "Muhammad Fadhil Arrayyan";
+let sekolah = "SMK Telkom Malang";
+let umur = 15;
+let hobi = "Bermain Catur";
+
+console.log(`Nama saya adalah ${nama}, saya sekarang bersekolah di ${sekolah}, saya berumur ${umur} tahun, hobi saya adalah ${hobi}.`);
